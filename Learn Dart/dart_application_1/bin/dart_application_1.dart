34817b6da3c6cf3c01b15612ac793344ef7a2c5d
Chintan Sandesara <chintan.sandesara@gmail.com>
2022-07-03 19:43:04 -0400

    committed

--- Learn Dart/dart_application_1/bin/dart_application_1.dart
+++ Learn Dart/dart_application_1/bin/dart_application_1.dart
@@ -2,4 +2,7 @@ import 'package:dart_application_1/dart_application_1.dart' as dart_application_
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
+
+  double temp = 38.457;
+  String tempText = temp
 }
